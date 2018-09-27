7e09d101a43b3d2c0e32457dfc02f35f4a0b6d04
hiraksarkar <hsarkar@cs.stonybrook.edu>
2018-09-27 12:04:45 -0400

    PR4: added simple script to detect/validate fusion due to read mapping to 2 different genes

---
+++ src/validateFusion.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <random>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout<<"Started\n\n";
+    string ln, referenceName, pos1, pos2, gene_chromosome_name, readSeq;
+    unordered_map<string, string> genomes;
+    unordered_map<string, unordered_set<string>> read_gene_map;
+    double countRefMatch = 0;
+    if(argc == 1)
+    {
+        cout<<"No input file specified\n"<<endl;
+        exit(1);
+    }
+    ifstream fl(argv[1]);
+    while(getline(fl, ln))
+    {
+        istringstream iss(ln);
+        if (!(iss >> referenceName >> gene_chromosome_name >> pos1 >> pos2 >> readSeq))
+            break;
+        if((gene_chromosome_name[0] == 'E') && (gene_chromosome_name[1] == 'N'))
+        {
+            string gene_for_the_current_read;
+            stringstream iss3(gene_chromosome_name);
+            if(getline(iss3, gene_for_the_current_read, '|'))
+                if(getline(iss3, gene_for_the_current_read, '|'))
+                    if(getline(iss3, gene_for_the_current_read, '|'))
+                        if(getline(iss3, gene_for_the_current_read, '|'))
+                            if(getline(iss3, gene_for_the_current_read, '|'))
+                                if(getline(iss3, gene_for_the_current_read, '|'))
+                                {
+                                    auto itr_read = read_gene_map.find(referenceName);
+                                    if(itr_read == read_gene_map.end())
+                                    {
+                                        unordered_set<string> gene_for_this_read;
+                                        gene_for_this_read.emplace(gene_for_the_current_read);
+                                        read_gene_map.emplace(referenceName, gene_for_this_read);
+                                    }
+                                    else
+                                        itr_read->second.emplace(gene_for_the_current_read);
+                                }
+        }
+    }
+    cout << "Fused genes found:\n";
+    for(const auto &genItr : read_gene_map)
+    {
+        if(genItr.second.size() > 1)
+        {
+            vector<string> vec_gene;
+            for(auto itr_mp = genItr.second.begin(); itr_mp != genItr.second.end(); itr_mp++)
+                vec_gene.push_back(*itr_mp);
+            // Assuming fusion by the reads mapping to 2 different genes
+            auto it_g1 = genomes.find(vec_gene[0]);
+            auto it_g2 = genomes.find(vec_gene[1]);
+            if(it_g1 != genomes.end())
+              if(it_g1->second == vec_gene[1])
+                continue;
+            if(it_g2 != genomes.end())
+              if(it_g2->second == vec_gene[0])
+                continue;
+            genomes.emplace(vec_gene[0], vec_gene[1]);
+            for(auto fused_genes : vec_gene)
+                cout << fused_genes << ",\t";
+            cout << endl;
+        }
+    }
+    cout << "\nTotal number of Gene Fusions detected: " << genomes.size() << endl;
+}
